28970d142c0675fc5d006d0ed17397fe4f4faf1e
nour-aldin <nourdx73@gmail.com>
2023-10-09 22:55:28 +0300

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
+  server: {
     port: 3000,
     // HANDEL CORS ERRORS
     proxy: {
       "/api": {
-        target: 'http://localhost:5000',
+        target: "https://threads-server.onrender.com",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
+      },
+    },
+  },
 })
